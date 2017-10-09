7febdd33ebb4b246f83b7a88a70f5ef70770378a
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-10-09 17:11:40 +0100

    adds html for cat image

--- lib/app.rb
+++ lib/app.rb
@@ -9,3 +9,27 @@ end
 get '/secret' do
   "This is the secret route"
 end
+
+get '/cat' do
+
+  '<div style="text-align:center;"><img src="http://bit.ly/1eze8aE" alt="cat image" style="border: medium dashed red;"></div>'
+  # <% end %>
+  #
+  # # <% <div class="fun-border">
+  #   <img src="http://bit.ly/1eze8aE" alt="cat image">
+  # </div>
+  #
+  # <style contentedible>
+  # .fun-border {
+  #   border: medium dashed red;
+  # }
+  # </style>
+  # %>
+  # <style>
+  # .picture{
+  # border: medium dashed red;
+  # }
+  # </style>
+
+
+end
